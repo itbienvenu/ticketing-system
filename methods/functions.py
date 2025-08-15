1a630268f6b8219eb3dbe95d3367377809e0a7d0
itbienvenu <bienvenugashema@gmail.com>
2025-08-16 00:46:42 +0200

    creating login with access token

--- methods/functions.py
+++ methods/functions.py
@@ -4,6 +4,24 @@ from passlib.hash import bcrypt
 from database.dbs import Base, engine, get_db
 from database.models import User
 from schemas.LoginRegisteScheme import RegisterUser, LoginUser
+from jose import jwt
+import os
+from dotenv import load_dotenv
+from datetime import timedelta, datetime, UTC
+
+load_dotenv()
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALGORITHM = os.environ.get("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.now(UTC) + (expires_delta or timedelta(minutes=15))   
+    to_encode.update({"exp": expire})   
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)  
+    return encoded_jwt
 
 
 def create_user(db: Session, user: RegisterUser):
@@ -25,3 +43,27 @@ def create_user(db: Session, user: RegisterUser):
     db.refresh(db_user)
     return db_user
 
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return bcrypt.verify(plain_password, hashed_password)
+
+def login_user(db: Session, user: LoginUser):
+
+    check_user = None
+
+    if user.email:
+        check_user = db.query(User).filter(User.email == user.email).first()
+
+    elif user.phone_number:
+        check_user = db.query(User).filter(User.phone_number == user.phone_number).first() 
+
+    if not check_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    if not verify_password(user.password, check_user.password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    token = create_access_token(data={"sub":str(check_user.id)}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    return {
+        "message":"Login Successful",
+        "access_token":token,
+        "torkn_type": "bearer"
+            }
--- routes/login_router.py
+++ routes/login_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
-from methods.functions import create_user
-from schemas.LoginRegisteScheme import RegisterUser
+from methods.functions import create_user, login_user
+from schemas.LoginRegisteScheme import RegisterUser, LoginUser
 from database.dbs import get_db
 from database.models import *
 from sqlalchemy.orm import Session
@@ -8,9 +8,10 @@ from sqlalchemy.orm import Session
 
 router = APIRouter(prefix="/api/v1", tags=["Login and Registratin"])
 
-@router.get("/login")
-async def login():
-    return {"message":"Login page"}
+@router.post("/login")
+async def login(user: LoginUser, db: Session = Depends(get_db)):
+    return login_user(db, user)
+
 
 @router.post("/register", response_model=RegisterUser)
 async def register(user: RegisterUser, db: Session = Depends(get_db)):
